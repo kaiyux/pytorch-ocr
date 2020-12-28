b782f636c43642cc44bc91c11ca16eee57d0674d
XIE <siriusxky@gmail.com>
2020-12-28 21:35:46 +0800

    Use ShuffleNet_v2 as backbone

---
+++ model/backbone/__init__.py
@@ -0,0 +1,5 @@
+from .shufflenet_v2 import ShuffleNetV2
+
+__all__ = [
+    'ShuffleNetV2'
+]
--- model/model.py
+++ model/model.py
@@ -1,8 +1,9 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-import torchvision.models as models
 from .attention import TransformerModel
+from .backbone import ShuffleNetV2
 
 
 class MnistModel(BaseModel):
@@ -25,12 +26,21 @@ class MnistModel(BaseModel):
 
 
 class RecognizeModel(BaseModel):
-    def __init__(self, num_chars):
+    def __init__(self, num_chars, d_model):
         super().__init__()
-        self.resnet50 = models.resnet50(pretrained=False, progress=True)
-        self.transformer = TransformerModel(num_chars, nhead=16, num_encoder_layers=12)
+        self.backbone = ShuffleNetV2()
+        self.embedding = nn.Embedding(num_embeddings=num_chars, embedding_dim=d_model)
+        self.transformer = TransformerModel(num_chars, d_model=d_model, nhead=16, num_encoder_layers=12)
 
     def forward(self, img, transcription):
-        features = self.resnet50(img)
+        features = self.backbone(img)
+        N, C, H, W = features.shape[0], features.shape[1], features.shape[2], features.shape[3]
+        features = features.permute(0, 1, 3, 2)
+        features = features.reshape(torch.Size([N, C, H * W]))  # (N, E, S)
+        features = features.permute(2, 0, 1)  # (N, E, S) -> (S, N, E)
+
+        transcription = self.embedding(transcription.long())  # (N, T) -> (N, T, E)
+        transcription = transcription.permute(1, 0, 2)  # (N, T, E) -> (T, N, E)
+
         outs = self.transformer(features, transcription)
         return outs
