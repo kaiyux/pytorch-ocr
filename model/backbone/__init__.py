933411bd9457ba684a5406d66c113b3cc288a3c4
kaiyu <siriusxky@gmail.com>
2021-02-21 22:48:33 +0800

    Add ResNet as one of backbone choices

--- model/backbone/__init__.py
+++ model/backbone/__init__.py
@@ -1,7 +1,9 @@
 from .shufflenet_v2 import ShuffleNetV2
 from .tinynet import TinyNet
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 
 __all__ = [
     'ShuffleNetV2',
     'TinyNet',
+    'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
 ]
