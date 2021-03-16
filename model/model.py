5f193411bb7accc6ff31ca03354dd8a5529a2e82
Kaiyu Xie <siriusxky@gmail.com>
2021-03-16 21:31:14 +0800

    Rename

--- model/model.py
+++ model/model.py
@@ -4,14 +4,14 @@ from .attention import TransformerModel, TransformerEncoderModel, TransformerDec
 from .lstm import LSTMModel
 from .backbone import ShuffleNetV2, TinyNet, resnet18, resnet34, resnet50
 
-backbone_dict = {
+arch_backbones = {
     'ShuffleNet': ShuffleNetV2,
     'ResNet18': resnet18,
     'ResNet34': resnet34,
     'ResNet50': resnet50
 }
 
-head_dict = {
+arch_heads = {
     'LSTM': LSTMModel,
     'Transformer': TransformerModel,
     'TransformerEncoder': TransformerEncoderModel,
@@ -23,19 +23,19 @@ class RecognizeModel(BaseModel):
     def __init__(self, num_chars, d_model, nhead, num_layers,
                  backbone='ResNet18', head='TransformerEncoder'):
         super().__init__()
-        assert backbone in backbone_dict.keys(), 'Invalid backbone.'
-        assert head in head_dict.keys(), 'Invalid head.'
+        assert backbone in arch_backbones.keys(), 'Invalid backbone.'
+        assert head in arch_heads.keys(), 'Invalid head.'
         if head in ['Transformer', 'TransformerDecoder']:
             self.has_decoder = True
         else:
             self.has_decoder = False
 
-        self.backbone = backbone_dict[backbone]()
+        self.backbone = arch_backbones[backbone]()
 
         if head == 'LSTM':
             self.head = LSTMModel(d_model, num_chars)
         else:
-            self.head = head_dict[head](num_chars, d_model, nhead, num_layers)
+            self.head = arch_heads[head](num_chars, d_model, nhead, num_layers)
 
     def forward(self, img, transcription=None):
         features = self.backbone(img)
